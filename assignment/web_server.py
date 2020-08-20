178c504b075d5b3cae65c47fa94100852af2b6d0
Nathan Wang <cont3nted>
2020-08-20 18:55:54 +0800

    Added Camera API using openCV(for webcam),code edited in facerecog & web_server

--- assignment/web_server.py
+++ assignment/web_server.py
@@ -1,41 +1,44 @@
-import gevent
-import gevent.monkey
-from gevent.pywsgi import WSGIServer
-gevent.monkey.patch_all()
 
 from flask import Flask, render_template, jsonify, request, Response, redirect, url_for, session, escape
-
-
+from flask_cors import CORS
 import argparse
 import sys
 import requests
 import time
-#import winsound
-
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
 import json
-
+import os
+import cv2
 from datetime import datetime
-
 from IOTAssignmentUtilitiesdorachua.MySQLManager import MySQLManager
 from IOTAssignmentUtilitiesdorachua.MySQLManager import QUERYTYPE_DELETE, QUERYTYPE_INSERT, QUERYTYPE_UPDATE
-
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
-
 import jsonconverter as jsonc
+from gevent.pywsgi import WSGIServer
+import gevent
+# import gevent.monkey
+# gevent.monkey.patch_all()
+
+
+#import winsound
+
 
 app = Flask(__name__)
+CORS(app)
 
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
 # telegram bot to send alerts to the analyst
+
+
 def telegram_bot(bot_message):
 
     bot_token = '1157671100:AAFnOQeaeRu6Jy0RsHzGf0Q9RUqUV7H4kbI'
     bot_chatID = '280242805'
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+    send_text = 'https://api.telegram.org/bot' + bot_token + \
+        '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
     response = requests.get(send_text)
 
@@ -43,10 +46,10 @@ def telegram_bot(bot_message):
 
 
 # speed limit 80, if more than 80 , send a alert message to the telegram bot
-@app.route("/api/sendalert",methods=['GET', 'POST'])
+@app.route("/api/sendalert", methods=['GET', 'POST'])
 def apidata_sendalert():
     try:
-        bookingid ="0.0"
+        bookingid = "0.0"
         speedkmhour = "0"
         day = "Monday 2020."
         speed = ""
@@ -54,7 +57,7 @@ def apidata_sendalert():
         frequency = 2000
         duration = 1000
 
-        if 'bookingid' in request.form:        
+        if 'bookingid' in request.form:
             bookingid = request.form['bookingid']
         if 'speedkmhour' in request.form:
             speedkmhour = request.form['speedkmhour']
@@ -63,55 +66,55 @@ def apidata_sendalert():
         if 'time' in request.form:
             time = request.form['time']
 
-
         speedkmhourf = float(speedkmhour)
 
-        message = "Hi driver of Booking ID: " + bookingid + "," + " you have been caught speeding at " + "{0:.2f}".format(speedkmhourf) + " KM/H on " + time + " on " + date + " Max speed limit is 80, please slow down."  
-        
+        message = "Hi driver of Booking ID: " + bookingid + "," + " you have been caught speeding at " + \
+            "{0:.2f}".format(speedkmhourf) + " KM/H on " + time + \
+            " on " + date + " Max speed limit is 80, please slow down."
+
         telegram_bot(message)
 
         #winsound.Beep(frequency, duration)
-        return redirect("/dashboard", code=303) #redirect to a page
+        return redirect("/dashboard", code=303)  # redirect to a page
     except:
         print(sys.exc_info()[0])
         print(sys.exc_info()[1])
 
-        
+
 # to show the booking id to the analyst when he/she searches.
-@app.route("/api/showbookingid",methods=['GET', 'POST'])
+@app.route("/api/showbookingid", methods=['GET', 'POST'])
 def apidata_showbookingid():
     try:
 
         table_name = 'grabtable'
         print(f"Querying table : {table_name}")
-        #define connection
+        # define connection
         dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
         table = dynamodb.Table(table_name)
 
         bookingid = '0.0'
-        if 'getbookingid' in request.form:        
-            bookingid = request.form['getbookingid']     
+        if 'getbookingid' in request.form:
+            bookingid = request.form['getbookingid']
 
-        #Querying from the table
+        # Querying from the table
         response = table.query(
-            #KeyConditionExpression=key('bookingid').eq('0.0')
-            #Add the name of the index you want to use in your query
-            #IndexName is something like a primary key
+            # KeyConditionExpression=key('bookingid').eq('0.0')
+            # Add the name of the index you want to use in your query
+            # IndexName is something like a primary key
             IndexName="bookingid-datetime_value-index",
             KeyConditionExpression=Key('bookingid').eq(bookingid),
             ScanIndexForward=False,
             Limit=10
         )
 
-
         items = response['Items']
 
-        n=10 #limit to last 10 items
-        data = items[:n] #slicing 
+        n = 10  # limit to last 10 items
+        data = items[:n]  # slicing
         data_reversed = data[::-1]
 
         return jsonify(json.loads(jsonc.data_to_json(data_reversed)))
-        
+
     except:
         print(sys.exc_info()[0])
         print(sys.exc_info()[1])
@@ -119,7 +122,7 @@ def apidata_showbookingid():
 
 # retrieves the data from db and sends it to a line graph and table.
 
-@app.route("/api/getdata",methods=['GET', 'POST'])
+@app.route("/api/getdata", methods=['GET', 'POST'])
 def apidata_getdata():
     try:
         table_name = 'grabtable'
@@ -127,14 +130,14 @@ def apidata_getdata():
         dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
         table = dynamodb.Table(table_name)
 
-        #response = table.query(
-            # Add the name of the index you want to use in your query.
+        # response = table.query(
+        # Add the name of the index you want to use in your query.
         #    IndexName="bookingid-datetime_value-index",
         #    KeyConditionExpression=Key('bookingid').eq('0.0'),
         #    ScanIndexForward=False,
         #    Limit=10
-        #)            
-        #to pull
+        # )
+        # to pull
         response = table.query(
             IndexName="sort-datetime_value-index",
             KeyConditionExpression=Key('sort').eq('1'),
@@ -144,18 +147,18 @@ def apidata_getdata():
 
         items = response['Items']
 
-        n=10 # limit to last 10 items
+        n = 10  # limit to last 10 items
         data = items[:n]
         data_reversed = data[::-1]
-        #print(data_reversed)
-        #print( (json.loads(jsonc.data_to_json(data_reversed)))
+        # print(data_reversed)
+        # print( (json.loads(jsonc.data_to_json(data_reversed)))
         return jsonify(json.loads(jsonc.data_to_json(data_reversed)))
     except:
         print(sys.exc_info()[0])
         print(sys.exc_info()[1])
 
 # route to handle the login
-@app.route("/api/login",methods=['GET', 'POST'])
+@app.route("/api/login", methods=['GET', 'POST'])
 def apidata_login():
     try:
 
@@ -173,25 +176,26 @@ def apidata_login():
             password = request.form['password']
 
         try:
-            
-            response = table.get_item(Key= {'username': username, 'password': password})
+
+            response = table.get_item(
+                Key={'username': username, 'password': password})
 
             items = response['Item']
             print(items)
-        
+
             if (items):
                 session['username'] = username
-                return redirect(url_for('dashboard', username = username), code = 303)
+                return redirect(url_for('dashboard', username=username), code=303)
         except Exception as e:
             print(e)
-            return render_template('login.html', msg = "Username or password is incorrect, please try again.")
-        
+            return render_template('login.html', msg="Username or password is incorrect, please try again.")
+
     except:
         print(sys.exc_info()[0])
-        print(sys.exc_info()[1]) 
+        print(sys.exc_info()[1])
 
 # route to handle register
-@app.route("/api/register",methods=['GET', 'POST'])
+@app.route("/api/register", methods=['GET', 'POST'])
 def apidata_register():
     try:
 
@@ -204,7 +208,7 @@ def apidata_register():
         name = 'username'
         msg = ''
         role = 'analyst'
-        # Check 
+        # Check
         if 'username' in request.form and 'email' in request.form and 'number' in request.form and 'password' in request.form and 'cpassword' in request.form:
             username = request.form['username']
             email = request.form['email']
@@ -213,13 +217,14 @@ def apidata_register():
             cpassword = request.form['cpassword']
 
         my_rpi = AWSIoTMQTTClient("registerUser")
-        
+
         host = "a19dfxc0pabiyn-ats.iot.us-east-1.amazonaws.com"
         rootCAPath = "certs/rootca.pem"
         certificatePath = "certs/users-certificate.pem.crt"
         privateKeyPath = "certs/users-private.pem.key"
         my_rpi.configureEndpoint(host, 8883)
-        my_rpi.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+        my_rpi.configureCredentials(
+            rootCAPath, privateKeyPath, certificatePath)
         my_rpi.connect()
         usersinfo = {}
         usersinfo['username'] = username
@@ -227,7 +232,7 @@ def apidata_register():
         usersinfo['number'] = number
         usersinfo['password'] = password
         usersinfo['role'] = role
-        #r = { "username": username, "email": email, "number": number, "password": password, 'role': role }   
+        #r = { "username": username, "email": email, "number": number, "password": password, 'role': role }
 
         success = my_rpi.publish("iot/users", json.dumps(usersinfo), 1)
         #my_rpi.publish("iot/users", json.dumps(r), 1)
@@ -235,16 +240,15 @@ def apidata_register():
             print("success")
         else:
             print("fail")
-        #if (userregister):
+        # if (userregister):
         session['username'] = username
-        return redirect(url_for('dashboard', username = username), code = 303)
-        #else:
-            #return render_template('register.html', msg = "Incorrect fields, please try again")
+        return redirect(url_for('dashboard', username=username), code=303)
+        # else:
+        # return render_template('register.html', msg = "Incorrect fields, please try again")
 
-        
     except:
         print(sys.exc_info()[0])
-        print(sys.exc_info()[1]) 
+        print(sys.exc_info()[1])
 
 # # route to handle the profile
 # @app.route("/api/profile",methods=['GET', 'POST'])
@@ -254,7 +258,7 @@ def apidata_register():
 #         name = username
 #         msg = ''
 #         role = 'analyst'
-#         # Check 
+#         # Check
 #         if 'username' in request.form and 'email' in request.form and 'number' in request.form and 'password' in request.form and 'cpassword' in request.form:
 #             username = request.form['username']
 #             email = request.form['email']
@@ -265,9 +269,9 @@ def apidata_register():
 #         u='iotuser';pw='iotpassword';h='localhost';db='iotdatabase'
 #         mysqlm = MySQLManager(u,pw,h,db)
 #         mysqlm.connect()
-            
+
 #         sql="UPDATE users SET %(username)s, %(email)s, %(number)s, %(password)s, %(role)s WHERE name = username"
-#         updatedetails = {"username": username, "email": email, "number": number, "password": password, "role": role}            
+#         updatedetails = {"username": username, "email": email, "number": number, "password": password, "role": role}
 #         userupdate = mysqlm.insertupdatedelete(QUERYTYPE_UPDATE,sql,updatedetails)
 #         mysqlm.disconnect()
 
@@ -276,13 +280,13 @@ def apidata_register():
 #             return redirect(url_for('dashboard', username = username), code = 303)
 #         else:
 #             return render_template('profile.html', msg = "Unable to update, please try again")
-        
+
 #     except:
 #         print(sys.exc_info()[0])
-#         print(sys.exc_info()[1]) 
-    
+#         print(sys.exc_info()[1])
+
 # this is to handle the dashboard data
-@app.route("/api/getdashboarddata",methods=['GET', 'POST'])
+@app.route("/api/getdashboarddata", methods=['GET', 'POST'])
 def apidata_getdashboarddata():
     try:
         table_name = 'grabtable'
@@ -292,7 +296,6 @@ def apidata_getdashboarddata():
 
         response = table.scan(AttributesToGet=['bookingid', 'speedkmhour'])
 
-        
         lst = []
         for i in response['Items']:
             lst.append(i['bookingid'])
@@ -303,23 +306,22 @@ def apidata_getdashboarddata():
         max_speed = []
         for i in response['Items']:
             max_speed.append(i['speedkmhour'])
-        
-        max_speed_value = max(max_speed)
 
+        max_speed_value = max(max_speed)
 
         r = {}
         r['driver_data'] = unique_booking_count
         r['max_speed'] = max_speed_value
-     
+
         return jsonify(json.loads(jsonc.data_to_json(r)))
-        #return dashboarddata
+        # return dashboarddata
 
     except:
         print(sys.exc_info()[0])
-        print(sys.exc_info()[1])     
+        print(sys.exc_info()[1])
 
 
-@app.route("/api/getbookingdashboarddata",methods=['GET', 'POST'])
+@app.route("/api/getbookingdashboarddata", methods=['GET', 'POST'])
 def apidata_getbookingdashboarddata():
     try:
         table_name = 'grabtable'
@@ -328,13 +330,13 @@ def apidata_getbookingdashboarddata():
         table = dynamodb.Table(table_name)
 
         bookingid = '0.0'
-        if 'getbookingid' in request.form:        
+        if 'getbookingid' in request.form:
             bookingid = request.form['getbookingid']
         response = table.scan(AttributesToGet=['bookingid', 'speedkmhour'])
         response2 = table.query(
-            #KeyConditionExpression=key('bookingid').eq('0.0')
-            #Add the name of the index you want to use in your query
-            #IndexName is something like a primary key
+            # KeyConditionExpression=key('bookingid').eq('0.0')
+            # Add the name of the index you want to use in your query
+            # IndexName is something like a primary key
             IndexName="bookingid-datetime_value-index",
             KeyConditionExpression=Key('bookingid').eq(bookingid),
             ScanIndexForward=False,
@@ -350,7 +352,7 @@ def apidata_getbookingdashboarddata():
         max_speed = []
         for i in response2['Items']:
             max_speed.append(i['speedkmhour'])
-        
+
         max_speed_value = max(max_speed)
         aver_speed_value = (sum(max_speed)/len(max_speed))
         aver_speed_value = round(aver_speed_value, 2)
@@ -359,9 +361,9 @@ def apidata_getbookingdashboarddata():
         r['driver_data'] = unique_booking_count
         r['average_speed'] = aver_speed_value
         r['max_speed'] = max_speed_value
-     
+
         return jsonify(json.loads(jsonc.data_to_json(r)))
-        #return dashboarddata
+        # return dashboarddata
 
     except:
         print(sys.exc_info()[0])
@@ -370,46 +372,86 @@ def apidata_getbookingdashboarddata():
 # @app.route("/api/getdashboarddata",methods=['GET', 'POST'])
 # def apidata_getdashboarddata():
 #     try:
-#         
-    
+#
 
 
 #         #items = response['Items']
 
 #         #n=10 # limit to last 10 items
 #         dashboarddata = {'driver_data': unique_booking_count, 'average_speed_data': unique_booking_count, 'max_speed': unique_booking_count}
-        
+
 #         return dashboarddata
 #     except:
 #         print(sys.exc_info()[0])
 #         print(sys.exc_info()[1])
+
+# Camera API
+# Take Picture using CV2 library
+@app.route("/api/camera", methods=['GET', 'POST'])
+def camera():
+    videoCaptureObject = cv2.VideoCapture(0)
+    result = True
+    date = datetime.now()
+    while (result):
+        ret, frame = videoCaptureObject.read()
+        path = './static/saved_images'
+
+        # datetime object containing current date and time
+        now = datetime.now()
+
+        # dd/mm/YY H:M:S
+        date_string = datetime.now().strftime('-%d-%m-%Y-%H-%M-%S')
+        cv2.imwrite(os.path.join(path, 'image'+date_string+'.jpg'), frame)
+        result = False
+        videoCaptureObject.release()
+        cv2.destroyAllWindows()
+        print('image saved!')
+    return(path)
+
+# Get Saved images
+@app.route("/api/getImages", methods=['POST'])
+def getImages():
+    from PIL import Image
+    import glob
+    image_list = []
+    for filename in glob.glob('./static/saved_images/*.jpg'):
+        image_list.append(filename)
+
+    print(image_list)
+
+    return(jsonify(image_list))
 
 
 @app.route("/multiple")
 def multiple():
     return render_template('index_multiple.html')
 
+
 @app.route("/")
 def home():
     return render_template('index.html')
+
 
 @app.route("/profile")
 def profile():
     if 'username' in session:
         username = escape(session['username'])
-    return render_template('profile.html', username = username)
+    return render_template('profile.html', username=username)
 
-@app.route("/showbookingid", methods = ['GET', 'POST'])
+
+@app.route("/showbookingid", methods=['GET', 'POST'])
 def showbookingid():
     if 'getbookingid' in request.form:
         booking_value = request.form['getbookingid']
     if 'username' in session:
         username = escape(session['username'])
-    return render_template('showbookingid.html', username = username, hidden_bookingid = booking_value)
+    return render_template('showbookingid.html', username=username, hidden_bookingid=booking_value)
+
 
 @app.route("/login")
 def login():
     return render_template('login.html')
+
 
 @app.route("/logout")
 def logout():
@@ -417,9 +459,11 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('login'))
 
+
 @app.route("/register")
 def register():
     return render_template('register.html')
+
 
 @app.route("/facialRecog")
 def facialRecog():
@@ -430,32 +474,33 @@ def facialRecog():
 def speedcheck():
     if 'username' in session:
         username = escape(session['username'])
-    return render_template('speedcheck.html', username = username)
+    return render_template('speedcheck.html', username=username)
+
 
 @app.route("/dashboard")
 def dashboard():
     if 'username' in session:
         username = escape(session['username'])
-    return render_template('dashboard.html', username = username)
+    return render_template('dashboard.html', username=username)
 
 
 if __name__ == '__main__':
-   try:
+    try:
         host = '0.0.0.0'
         port = 5000
-        parser = argparse.ArgumentParser()        
-        parser.add_argument('port',type=int)
-        
+        parser = argparse.ArgumentParser()
+        parser.add_argument('port', type=int)
+
         args = parser.parse_args()
         if args.port:
             port = args.port
-                
+
         http_server = WSGIServer((host, port), app)
         app.debug = True
         print('Web server is now waiting for requests')
         http_server.serve_forever()
 
-   except:
+    except:
         print("Exception occured while running web server!")
         print(sys.exc_info()[0])
         print(sys.exc_info()[1])
